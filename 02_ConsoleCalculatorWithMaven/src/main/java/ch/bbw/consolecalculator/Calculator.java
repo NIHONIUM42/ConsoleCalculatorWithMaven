e4bcae9639620c36b9e5fe5814093ad6bfb9044a
Jovan Bühler <jovan.manuel.buehler@gmail.com>
2019-01-01 08:41:51 +0100

    initial commit

---
+++ 02_ConsoleCalculatorWithMaven/src/main/java/ch/bbw/consolecalculator/Calculator.java
@@ -0,0 +1,7 @@
+package ch.bbw.consolecalculator;
+
+public class Calculator {
+	public int summe(int summand00, int summand01) {
+		return summand00 + summand01;
+	}
+}
---
+++ 02_ConsoleCalculatorWithMaven/src/main/java/ch/bbw/consolecalculator/Snippet.java
@@ -0,0 +1,8 @@
+package ch.bbw.consolecalculator;
+
+public class Snippet {
+	public static void main(String[] args) {
+		ConsoleCalculator02
+	}
+}
+
