2b3fbcf10a311a87c3db07a5c8bde13af2eb8e34
NIHONIUM42 <jovan.manuel.buehler@gmail.com>
2019-01-14 08:18:23 +0100

    alles von neuem aufbauen

--- 02_ConsoleCalculatorWithMaven/src/main/java/ch/bbw/consolecalculator/Calculator.java
+++
@@ -1,7 +0,0 @@
-package ch.bbw.consolecalculator;
-
-public class Calculator {
-	public int summe(int summand00, int summand01) {
-		return summand00 + summand01;
-	}
-}
--- 02_ConsoleCalculatorWithMaven/src/main/java/ch/bbw/consolecalculator/Snippet.java
+++
@@ -1,8 +0,0 @@
-package ch.bbw.consolecalculator;
-
-public class Snippet {
-	public static void main(String[] args) {
-		ConsoleCalculator02
-	}
-}
-
