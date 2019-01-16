f11dafe5b563c7f88610ca40ecc886a1a705d5c6
NIHONIUM42 <jovan.manuel.buehler@gmail.com>
2019-01-16 10:58:03 +0100

    Git shall add the calculator class

---
+++ 02_ConsoleCalculatorWithMaven/src/main/java/ch/bbw/consolecalculator/Calculator.java
@@ -0,0 +1,15 @@
+package ch.bbw.consolecalculator;
+
+/*
+ * Calculator
+ * - kann zwei Zahlen addieren
+ * 
+ * @author Jovan Bühler
+ * @version 0.0.1
+ */
+
+public class Calculator {
+	public int summe(int summand1, int summand2) {
+		return summand1 + summand2;
+	}
+}
